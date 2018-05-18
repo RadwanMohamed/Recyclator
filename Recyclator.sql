cd89a687c856e692f50a448abe7d1217f022ce17
RadwanMohamed <radwanmohamed334@gmail.com>
2018-05-18 11:05:59 +0200

    edit deploy

---
+++ Recyclator.sql
@@ -0,0 +1,11 @@
+-- MySQL dump 10.16  Distrib 10.1.25-MariaDB, for Linux (x86_64)
+--
+-- Host: localhost    Database: dbname
+-- ------------------------------------------------------
+-- Server version	10.1.25-MariaDB
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO,POSTGRESQL' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
