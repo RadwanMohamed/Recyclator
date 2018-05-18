bd9796e5a638794fa1639d3143274771bb2accbd
RadwanMohamed <radwanmohamed334@gmail.com>
2018-05-18 13:21:07 +0200

    re edit

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: radwan
+ * Date: 28/02/18
+ * Time: 11:09 AM
+ */
+
+/*
+ * system settings
+ */
+$config['displayErrorDetails'] = true;
+$config['addContentLengthHeader'] = false;
+$config['debug'] = true;
+
+//access database info
+
+$config['db']['driver']   = 'mysql';
+$config['db']['host']   = 'db4free.net';
+$config['db']['port']   = '3306';
+$config['db']['database'] = 'recyclatordb';
+$config['db']['username']   = 'radwanadmin';
+$config['db']['password']   = 'radwan4dash';
+$config['db']['charset']   = 'utf8';
+$config['db']['collation']   = 'utf8_unicode_ci';
+$config['db']['prefix']   = '';
+
